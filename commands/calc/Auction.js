0b99600b217f6c4cd1dd5e9620b2dbd346366d3f
Astorion <xyorhzhf@gmail.com>
2023-04-14 12:57:41 +0900

    add test js

--- commands/calc/Auction.js
+++ commands/calc/Auction.js
@@ -7,14 +7,28 @@ module.exports = {
         .addIntegerOption(option =>
             option
                 .setName('가격')    
-                .setDescription('경매장 가격')
-                .setRequired(true)
-        ),
+                .setDescription('거래소 가격')
+                .setRequired(true))
+        ,
 	async execute(interaction) {
         const auctionCost = interaction.options.getInteger('가격');
-        const charge = 0.95;
-        const fairCost = auctionCost * charge
+        const auctionCostInCharge = auctionCost * 0.95;
 
-        await interaction.reply("4인: " + (fairCost - (fairCost / 4)) + "\n8인: " + (fairCost - (fairCost / 8)));
+        const fairCost4 = Math.floor(auctionCostInCharge / 4) * 3;
+        const fairCost8 = Math.floor(auctionCostInCharge / 8) * 7;
+        const fairCost16 = Math.floor(auctionCostInCharge / 16) * 15;
+        const bidCost4 = Math.floor(fairCost4 / 1.1);
+        const bidCost8 = Math.floor(fairCost8 / 1.1);
+        const bidCost16 = Math.floor(fairCost16 / 1.1);
+
+        await interaction.reply(
+            "\n거래소 가격: " + auctionCost +
+            "\n\nN빵 입찰가(4인): " + fairCost4 + " 이득: " + (Math.floor(auctionCostInCharge) - fairCost4) +
+            "\nN빵 선점 입찰가(4인): " + bidCost4 + " 이득: " + (Math.floor(auctionCostInCharge) - bidCost4) +
+            "\n\nN빵 입찰가(8인): " + fairCost8 + " 이득: " + (Math.floor(auctionCostInCharge) - fairCost8) +
+            "\nN빵 선점 입찰가(8인): " + bidCost8 + " 이득: " + (Math.floor(auctionCostInCharge) - bidCost8) +
+            "\n\nN빵 입찰가(16인): " + fairCost16 + " 이득: " + (Math.floor(auctionCostInCharge) - fairCost16) +
+            "\nN빵 선점 입찰가(16인): " + bidCost16 + " 이득: " + (Math.floor(auctionCostInCharge) - bidCost16)
+        );
 	},
 };
